59b9ccbc73679d112dbe12987676a3411e6c9fda
Marc Gener de Miguel <75481958+marcgenerdemiguel@users.noreply.github.com>
2022-05-13 17:25:56 +0200

    Update streamlit_app.py
    other lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale')
+streamlit.text('Hard-Boiled')
