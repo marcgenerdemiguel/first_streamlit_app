99417a2c220a2a85c873cd8e8786b47be7de82c6
Marc Gener de Miguel <75481958+marcgenerdemiguel@users.noreply.github.com>
2022-05-25 19:56:18 +0200

    Update streamlit_app.py
    typo text_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 streamlit.title('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text.input('What fruit would you like information about?', 'Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 
